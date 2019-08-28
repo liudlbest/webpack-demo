b7635f7b4b963aa4105f6aa117ad529a560b989c
liudl <liudlbest@126.com>
2019-08-29 07:30:23 +0800

    modified main.js

--- app/main.js
+++ app/main.js
@@ -3,7 +3,8 @@
 // document.querySelector("#root").appendChild(greeter());
 import React from 'react';
 import {render} from 'react-dom';
-
 import Greeter from './Greeter';
+
+import './main.css'; 
 
 render(<Greeter/>, document.getElementById('root'));
