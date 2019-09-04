f117399eb72bb1a254d21d8fbe970076d60a535d
liudl <liudlbest@126.com>
2019-09-04 10:51:32 +0800

    resolve postcss.config.js webpack.config.js conflicts

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 module.exports = {
     plugins: [
         require('autoprefixer')
     ]
-=======
-module.exports = { 
-    plugins: { 
-      'autoprefixer': {browsers: 'last 5 version'} 
-    } 
->>>>>>> dev
 }
