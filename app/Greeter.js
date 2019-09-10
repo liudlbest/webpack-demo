37341d565fa6072fca19213df6e637df03f773b4
liudlbest <liudlbest@126.com>
2019-09-10 22:38:59 +0800

    redux test1

--- app/Greeter.js
+++ app/Greeter.js
@@ -9,10 +9,25 @@ import styles from './Greeter.css';//导入
 //     return greet;
 //   };
 class Greeter extends Component{
+    constructor(){
+        super();
+        console.log("------Greeter constructor ----");
+
+        this.state = {
+            articles: [
+                {title: "React Redux Tutorial for Beginners", id: 1},
+                {title: "Redux e React: cos'e Reux e come usarlocon React", id:2}
+            ]
+        };
+    }
     render(){
+        const {articles} = this.state;
         return (
             <div className={styles.root}>//使用cssModule添加类名的方法
                 {config.greetText}
+                <ul>
+                    {articles.map(el => <li key={el.id}>{el.title}</li>)}
+                </ul>
             </div>
         );
     }
